dfae8bb7c0204829140121ecd7dbee186bc41a32
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-04-08 01:32:26 +0530

    Updated

--- Day1/string.js
+++ Day1/string.js
@@ -2,4 +2,9 @@ const nem = 'Joy Chakraborty';
 const aga = 20;
 console.log(`I am ${nem} and I'm ${aga}`);
 const q = 'Joy Chakraborty'
-console.log(q.length.toUpperCase());
+console.log(q.length);
+console.log(q.toUpperCase());
+console.log(q.substring(0,3));
+console.log(q.split(''));
+const hehe = 'bisi, mc, bhc';
+console.log(hehe.split(', '))
